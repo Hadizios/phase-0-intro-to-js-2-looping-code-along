55dd54fe5c39e262b9e3d903e08b5ebe20e5817c
Hadizios <hadi.maleh@gmail.com>
2023-02-17 20:36:00 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+writeCards(Cards);
+const names = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+function countDown(i) {
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
